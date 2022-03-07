0f780049726d354ac8e6aca059f3fc8ad037ea14
IsraelChidera <israelchidera54@gmail.com>
2022-03-07 23:17:19 +0100

    Single Country API data fetching and display

--- src/components/Country.js
+++ src/components/Country.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom';
 import { AiOutlineArrowLeft } from 'react-icons/ai';
 import axios from 'axios';
-
+import './Country.css';
 
 const Country = () => {
     const [country, setCountry] = useState([]);
@@ -15,7 +15,7 @@ const Country = () => {
                 console.log(response.data);               
             })
     }, [])
-    
+
   return (
     <section>
         <div>
@@ -23,7 +23,117 @@ const Country = () => {
                 <AiOutlineArrowLeft/> {" "} Back
             </Link>
         </div>
-        Country
+
+        <div className="singleCountryPage">
+            {country.map((count, i)=> {
+                const {flag, name, nativeName,
+                    population, region, subregion, capital,
+                    latlng, currencies, languages,
+                    timezones
+                } = count
+
+                return (
+                    <div
+                        className="singleCountry" 
+                        key={i}
+                    >
+                        <div className="countryFlag">
+                            <img src={flag}/>
+                        </div>
+
+                        <div>
+                            <h1>
+                                {name}
+                            </h1>
+
+                            <div className="countryDetailsFlexed">
+                                <div>
+                                    <p>
+                                        <span className="spanTopic">
+                                            Native Name:
+                                        </span>{" "} 
+                                        {nativeName}
+                                    </p>
+
+                                    <p>
+                                        <span className="spanTopic">
+                                            Population:
+                                        </span>{" "}
+                                        {population}
+                                    </p>
+
+                                    <p>
+                                        <span className="spanTopic">
+                                            Region:
+                                        </span>{" "}
+                                        {region}
+                                    </p>
+
+                                    <p>
+                                        <span className="spanTopic">
+                                            Sub Region:
+                                        </span>{" "}
+                                        {subregion}
+                                    </p>
+
+                                    <p>
+                                        <span className="spanTopic">
+                                            Capital:
+                                        </span>{" "}
+                                        {capital}
+                                    </p>
+                                </div>
+
+                                <div>
+                                    <p>
+                                        <span className="spanTopic">
+                                            Lat. & Long.:
+                                        </span>{" "}
+                                        {latlng.map((l)=>(
+                                            <span>
+                                                {l}{" "}
+                                            </span>
+                                        ))}
+                                    </p>
+
+                                    <p>
+                                        <span className="spanTopic">
+                                            Currencies:
+                                        </span>{" "}
+                                        {currencies.map((c)=>(
+                                            <span>
+                                                {c.name}
+                                            </span>
+                                        ))}
+                                    </p>
+
+                                    <p>
+                                        <span className="spanTopic">
+                                            Languages:
+                                        </span>{" "}
+                                        {languages.map((l)=>(
+                                            <span className="spanTopic">
+                                                {l.name}{" "}
+                                            </span>
+                                        ))}
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div>
+                                <p>
+                                    <span className="spanTopic">
+                                        Timezones: 
+                                    </span> {" "}
+                                    {timezones}
+                                </p>
+                            </div>
+                        </div>
+                        
+                    </div>
+                )
+            })}
+        </div>
     </section>
   )
 }
